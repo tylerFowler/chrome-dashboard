99d66ec6588bc131c47869745dd37c7a16e79cc9
Tyler Fowler <tylerfowler.1337@gmail.com>
2017-12-03 01:43:22 -0600

    Add sections

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ClockContainer from '../containers/ClockContainer';
+import styled from 'styled-components';
+
+const BaseSection = styled.div`
+  display: flex;
+  outline: 1px solid black;
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  margin: 0 5px;
+  height: 100vh;
+`;
+
+const LeftSection = styled(BaseSection)`
+  flex: 3;
+`;
+
+const MidSection = styled(BaseSection)`
+  flex: 3.5;
+`;
+
+const RightSection = styled(BaseSection)`
+  flex: 3;
+`;
 
 const App = () =>
-  <ClockContainer />
+  <div style={{display: 'flex', background: '#f0f0f0' }}>
+    <LeftSection></LeftSection>
+
+    <MidSection>
+      <ClockContainer />
+    </MidSection>
+
+    <RightSection></RightSection>
+  </div>
 ;
 
 export default App;
