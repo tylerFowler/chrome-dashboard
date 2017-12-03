de8312662e15d1c7f49e4361ff783bffb2384e0b
Tyler Fowler <tylerfowler.1337@gmail.com>
2017-12-03 01:43:43 -0600

    Add clock

--- app/components/Clock.js
+++ app/components/Clock.js
@@ -1,4 +1,56 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const ClockContainer = styled.div`
+  width: 75%;
+  max-width: 400px;
+  min-width: 350px;
+  height: 9em;
+  display: block;
+  margin: -3px auto;
+  border: 3px solid #4f4f4f;
+  background: #ffffff;
+
+  text-align: center;
+  color: #404040;
+`;
+
+const TimeContainer = styled.div`
+  font-family: Montserrat, sans-serif;
+`;
+
+const Time = styled.h1`
+  font-size: 4.5rem;
+  display: inline-block;
+  margin: 0;
+  margin-left: .75em;
+  letter-spacing: .1em;
+`;
+
+const TimePeriod = styled.span`
+  vertical-align: top;
+  display: inline-block;
+  font-size: 2rem;
+
+  position: relative;
+  top: .5em;
+`;
+
+const ClockDivider = styled.div`
+  width: 60%;
+  height: 0;
+  margin: 0 auto;
+
+  background: none;
+  border-bottom: 3px dashed #767676;
+`;
+
+const ClockDate = styled.h2`
+  font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+  font-weight: 200;
+  text-transform: uppercase;
+  margin: .5em auto;
+`;
 
 export default class Clock extends Component {
   constructor(props) { super(props); }
@@ -7,18 +59,18 @@ export default class Clock extends Component {
     const { time, date } = this.props;
 
     return (
-      <div>
-        <div>
-          <h1>{time.hour}:{time.minute}</h1>
-          <span>{time.modifier}</span>
-        </div>
+      <ClockContainer>
+        <TimeContainer>
+          <Time>{time.hour}:{time.minute}</Time>
+          <TimePeriod>{time.period}</TimePeriod>
+        </TimeContainer>
 
-        <hr />
+        <ClockDivider />
 
         <div>
-          <h2>{date.month} {date.day}, {date.year}</h2>
+          <ClockDate>{date.month} {date.day}, {date.year}</ClockDate>
         </div>
-      </div>
+      </ClockContainer>
     );
   }
 }
