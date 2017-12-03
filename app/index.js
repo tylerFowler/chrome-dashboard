79f9742e466b3538d408f9126cb185b4c26e7413
Tyler Fowler <tylerfowler.1337@gmail.com>
2017-12-03 00:23:21 -0600

    Add global styles

--- app/index.js
+++ app/index.js
@@ -3,10 +3,18 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store.js';
+import styled from 'styled-components';
+
+const AppStyled = styled(App)`
+  font-size: 16px;
+  font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+
+  background: #f0f0f0;
+`;
 
 render(
   <Provider store={store}>
-    <App />
+    <AppStyled />
   </Provider>,
   document.getElementById('app-container')
 );
