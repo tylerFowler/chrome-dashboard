54b638411d6498c0e88940f9a710a5800197732a
Tyler Fowler <tylerfowler.1337@gmail.com>
2017-12-29 19:37:54 -0600

    Add base Card styled component

---
+++ app/components/styled/Card.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: block;
+  min-width: 250px;
+  height: auto;
+
+  background: #ffffff;
+  border: 1px solid #4f4f4f;
+`;
+
+export default Card;
