a14656d7ca2882f7ccd46be9a11f3ba3c23bdd26
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-01-06 20:10:45 -0600

    Use font scale

--- lib/styles.ts
+++ lib/styles.ts
@@ -1,5 +1,17 @@
+import { Ratio, Scale } from 'natural-scale';
+
 export const fontSize = 16;
 export const fontFamily = "Lora, 'Times New Roman', Times, Georgia, serif";
+
+export const typeScale = Scale({
+  base: fontSize,
+  interval: Ratio.MAJOR_THIRD,
+  unit: 'em',
+});
+
+export const fontStacks = {
+  Montserrat: 'Montserrat, Helvetica, Verdana, sans-serif',
+};
 
 export const colors = {
   backgroundMain: '#f0f0f0',
