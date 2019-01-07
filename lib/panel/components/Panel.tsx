8a6d2995abe3585846918ccc6a1773a5c0f05f75
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-01-06 22:13:54 -0600

    Add Panel component

---
+++ lib/panel/components/Panel.tsx
@@ -0,0 +1,14 @@
+import styled from 'lib/styled-components';
+import React from 'react';
+
+const PanelContainer = styled.section``;
+
+export default class Panel extends React.Component {
+  public render() {
+    return (
+      <PanelContainer>
+        Panel
+      </PanelContainer>
+    );
+  }
+}
