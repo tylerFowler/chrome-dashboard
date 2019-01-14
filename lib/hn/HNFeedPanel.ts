baa11943d6ba19f6f5eddff67bf9e5f82a503483
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-01-14 01:37:08 -0600

    Pass state from redux state

--- lib/hn/HNFeedPanel.ts
+++ lib/hn/HNFeedPanel.ts
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
 import { GlobalState } from '../store';
-import { startAutoRefresh, stopAutoRefresh } from './actions';
+import { fetchPosts, startAutoRefresh, stopAutoRefresh } from './actions';
 import HNFeedPanel, { HNFeedPanelProps } from './components/HNFeedPanel';
+import { getStoryPage, isLoadingStories } from './selectors';
 
-const mapStateToProps = (_: GlobalState, ownProps: Partial<HNFeedPanelProps>): Partial<HNFeedPanelProps> => ({
+const mapStateToProps = (state: GlobalState, ownProps: Partial<HNFeedPanelProps>): Partial<HNFeedPanelProps> => ({
   ...ownProps,
+  loading: isLoadingStories(state),
+  stories: getStoryPage(15, state),
 });
 
 const mapDispatchToProps = (dispatch: Function): Partial<HNFeedPanelProps> => ({
+  fetchPosts() { dispatch(fetchPosts()); },
   // TODO get interval time from settings
   startHNFeedRefresh() { dispatch(startAutoRefresh(5 * 60 * 1000)); },
   stopHNFeedRefresh() { dispatch(stopAutoRefresh()); },
