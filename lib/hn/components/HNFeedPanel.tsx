cbd33767e6061f98794f7704a91f2f431ebd21a3
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-01-14 01:36:57 -0600

    Add feed items, loading, stories props

--- lib/hn/components/HNFeedPanel.tsx
+++ lib/hn/components/HNFeedPanel.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import FeedItem from '../../panel/components/FeedItem';
 import FeedPanel, { FeedProps } from '../../panel/components/FeedPanel';
+import { HNPost } from '../reducer';
 import hnTheme from '../theme';
 
 export interface HNFeedPanelProps extends FeedProps {
   title: never;
+  readonly loading: boolean;
+  readonly stories: ReadonlyArray<HNPost>;
+  fetchPosts(): void;
   startHNFeedRefresh(): void;
   stopHNFeedRefresh(): void;
 }
 
 export default class HNFeedPanel extends React.Component<HNFeedPanelProps> {
-  public componentDidMount() { this.props.startHNFeedRefresh(); }
-  public componentWillUnmount() { this.props.stopHNFeedRefresh(); }
+  public componentDidMount() {
+    this.props.fetchPosts();
+    this.props.startHNFeedRefresh();
+  }
+
+  public componentWillUnmount() {
+    this.props.stopHNFeedRefresh();
+  }
 
   public render() {
     return (
-      <FeedPanel {...this.props} title="Hacker News" theme={hnTheme} />
+      <FeedPanel {...this.props} title="Hacker News" theme={hnTheme}>
+        {this.props.stories.map((post, idx) =>
+          <li key={idx}>
+            <FeedItem
+              index={idx + 1} key={idx}
+              title={post.title}
+              url={post.url}
+              upvotes={post.score}
+              author={post.author}
+              commentCount={post.commentCount}
+            />
+          </li>,
+        )}
+      </FeedPanel>
     );
   }
 }
