4333f1998be56bcd749dd860d17a8cc6efe9d6b0
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-01-27 23:11:44 -0600

    Remove mock data

--- lib/panel/components/FeedPanel.tsx
+++ lib/panel/components/FeedPanel.tsx
@@ -1,6 +1,5 @@
 import styled from 'panel/styled-components';
 import React from 'react';
-import FeedItem, { FeedItemProps } from './FeedItem';
 import Panel, { PanelProps } from './Panel';
 import Spinner from './Spinner';
 
@@ -18,26 +17,9 @@ const FeedList = styled.ul`
   li { margin-bottom: 1em; }
 `;
 
-const feedData: FeedItemProps[] = [
-  { index: 1, title: 'New logo for city of Houston (USA)',
-    upvotes: 13, author: 'Catalin Cimpanu', commentCount: 8 },
-  { index: 2, title: 'Site Design: Barcamp Omaha',
-    upvotes: 4, author: 'Alex Van-Buren', commentCount: 4 },
-  { index: 3, title: 'Rio 2016 Olympics Branding',
-    upvotes: 7, author: 'Catalin Campanu', commentCount: 6 },
-  { index: 4, title: 'Ask DN: What side projects are you working on this weekend?',
-    upvotes: 11, author: 'Jonathan Svrdn', commentCount: 49, commentLink: 'https://google.com' },
-  { index: 5, title: 'The climate movements needs rebranding, but these Milton House cookies are too good',
-    upvotes: 4, author: 'Mark Massie', commentCount: 1 },
-].map(itm => ({ ...itm, id: itm.index, url: 'https://google.com' }));
-
 export default class FeedPanel extends React.Component<FeedProps> {
   public render() {
     const { loading, children, ...panelProps } = this.props;
-
-    const itms = feedData.map(itm => <li key={itm.index}>
-      <FeedItem {...itm} />
-    </li>);
 
     return (
       <Panel {...panelProps}>
@@ -45,7 +27,7 @@ export default class FeedPanel extends React.Component<FeedProps> {
 
         {!loading &&
           <FeedList>
-            {children || itms}
+            {children}
           </FeedList>
         }
       </Panel>
