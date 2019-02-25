0749807731be7807dd914668e8642dee8f0a7d95
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-02-24 22:14:19 -0600

    Add monospace font stack

--- lib/styles.ts
+++ lib/styles.ts
@@ -10,6 +10,7 @@ export const fontStacks = {
   Montserrat: 'Montserrat, Helvetica, Verdana, sans-serif',
   OpenSans: "'Open Sans', 'Helvetica Neue', Helvetica, Verdana, sans-serif",
   Lora: "Lora, 'Times New Roman', Times, Georgia, serif",
+  Monospace: "'Roboto Mono', 'Lucida Console', Monaco, monospace",
 };
 
 // TODO consider moving these to the theme definition
