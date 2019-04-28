39d93ebcd20d4976278dd1c33135e87d239fe727
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-04-28 01:53:47 -0500

    Do more explicit check on correct feed type in use

--- lib/settings/containers/HNFeedPanelSettings.ts
+++ lib/settings/containers/HNFeedPanelSettings.ts
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import { GlobalState } from '../../store';
-import HNSettings, { HNSettingsProps } from '../components/HNSettings';
 import { getPanelFeedSettings, getPanelFeedType } from '../selectors';
+import { FeedType } from '../../settings/interface';
 import { HNFeedSettings } from '../interface';
 import { updatePanelConfig } from '../actions';
+import HNSettings, { HNSettingsProps } from '../components/HNSettings';
 
 const mapStateToProps = (state: GlobalState, ownProps: Partial<HNSettingsProps>): Partial<HNSettingsProps> => {
-  if (!getPanelFeedType(ownProps.panelOrientation, state)) {
+  if (getPanelFeedType(ownProps.panelOrientation, state) !== FeedType.HN) {
     return {};
   }
 
