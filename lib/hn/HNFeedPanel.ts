0bc016c4f25adb14c105d83ebf521fd0c04093e0
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-04-28 01:40:34 -0500

    Add note about refrsh IDs

--- lib/hn/HNFeedPanel.ts
+++ lib/hn/HNFeedPanel.ts
@@ -11,7 +11,10 @@ const mapStateToProps = (state: GlobalState, ownProps: Partial<HNFeedPanelProps>
   stories: getStoryPage(15, state), // TODO: add 'feed item count' setting, populate here
 });
 
-// TODO: try and use the shorthand
+// TODO: once startAutoRefresh takes an ID, we'll want to generate it in this func
+// so that the component doesn't have to worry about it. Either using the ownProps
+// to build the ID or a rando number/incrementor from a memoized function, because
+// multiple calls of this needs to produce the same value for the same component.
 const mapDispatchToProps = (dispatch: Function): Partial<HNFeedPanelProps> => ({
   fetchPosts(feed) { dispatch(fetchPosts(feed)); },
   startHNFeedRefresh(refreshIval, feed) { dispatch(startAutoRefresh(refreshIval, feed)); },
