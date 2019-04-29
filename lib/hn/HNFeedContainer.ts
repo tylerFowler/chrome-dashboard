2c93ed74d7dbabf14a79ebf57b1a045861e42a6a
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-04-28 21:00:36 -0500

    Remove done todo

--- lib/hn/HNFeedContainer.ts
+++ lib/hn/HNFeedContainer.ts
@@ -7,10 +7,6 @@ const mapStateToProps = (_: GlobalState, ownProps: Partial<Props>): Partial<Prop
   ...ownProps,
 });
 
-// TODO: once startAutoRefresh takes an ID, we'll want to generate it in this func
-// so that the component doesn't have to worry about it. Either using the ownProps
-// to build the ID or a rando number/incrementor from a memoized function, because
-// multiple calls of this needs to produce the same value for the same component.
 const mapDispatchToProps = (dispatch: Function, ownProps: Pick<Props, 'panelOrientation'>): Partial<Props> => ({
   fetchPosts(feed, pullSize) { dispatch(fetchPosts(feed, pullSize)); },
   startHNFeedRefresh(refreshIval, feed, pullSize) {
