569a306e900d3a8c8cf0c1fa53d12d95676d97af
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-05-04 20:55:03 -0500

    Split up individual location types, add type guards

--- lib/settings/interface.ts
+++ lib/settings/interface.ts
@@ -31,9 +31,48 @@ export enum WeatherLocationType {
   Current = 'current',
 }
 
-export interface WeatherLocation {
-  type: WeatherLocationType;
-  value?: string;
-  countryCode?: string;
-  displayName?: string;
+export namespace WeatherLocation {
+  export interface City {
+    type: WeatherLocationType.CityName;
+    value: string;
+    countryCode?: string;
+    displayName?: string;
+  }
+
+  export const isCity = (loc: WeatherLocation): loc is City =>
+    loc.type === WeatherLocationType.CityName
+    && !!loc.value
+  ;
+
+  export interface ZIPCode {
+    type: WeatherLocationType.ZIPCode;
+    value: string;
+    countryCode: string;
+    displayName?: string;
+  }
+
+  export const isZIPCode = (loc: WeatherLocation): loc is ZIPCode =>
+    loc.type === WeatherLocationType.ZIPCode
+    && !!loc.value
+    && !!loc.countryCode
+  ;
+
+  export interface Coords {
+    type: WeatherLocationType.Coords|WeatherLocationType.Current;
+    value: string;
+    displayName?: string;
+    countryCode?: string; // must keep this to maintain prop parity with other types
+  }
+
+  export const isCoords = (loc: WeatherLocation): loc is Coords =>
+    loc.type === WeatherLocationType.Coords
+    && !!loc.value
+    && !loc.countryCode
+  ;
 }
+
+export type WeatherLocation =
+  | WeatherLocation.City
+  | WeatherLocation.ZIPCode
+  | WeatherLocation.Coords
+;
