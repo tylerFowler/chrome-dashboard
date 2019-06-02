45dfd32f41f0e287eb6e9519e892273816ab971e
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-06-02 18:31:44 -0500

    Wire up fetch forecast saga with test data

--- lib/weather/sagas.ts
+++ lib/weather/sagas.ts
@@ -1,1 +1,17 @@
-export default function* rootSaga() {}
+import { takeEvery, put } from 'redux-saga/effects';
+import { ActionType } from 'typesafe-actions';
+import { Actions, fetchForecast as fetchForecastAction, recvForecast } from './actions';
+
+function* fetchForecast(_: ActionType<typeof fetchForecastAction>) {
+  yield put(recvForecast({
+    condition: 'clearDay',
+    temperature: 80,
+  }, {
+    condition: 'clearNight',
+    temperature: 65,
+  }));
+}
+
+export default function* rootSaga() {
+  yield takeEvery(Actions.FetchForecast, fetchForecast);
+}
