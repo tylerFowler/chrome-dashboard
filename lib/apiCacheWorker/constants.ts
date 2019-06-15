911faa47879a85441a056e41d844088d8f3a20a1
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-06-15 15:08:15 -0500

    Rename feed bucket key to 'feeds'

--- lib/apiCacheWorker/constants.ts
+++ lib/apiCacheWorker/constants.ts
@@ -2,5 +2,5 @@
 // time, in milliseconds.
 export const defaultCacheExpirationThreshold = 5 * 60 * 1000; // 5 minutes
 
-export const feedBucketKey = 'feed' as const;
+export const feedBucketKey = 'feeds' as const;
 export const weatherBucketKey = 'weather' as const;
