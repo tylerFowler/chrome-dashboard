402413191847518dc07bf1aeb0b0a56a9683727f
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-06-15 17:38:54 -0500

    Debounce incoming location changes when updating preview

--- lib/settings/components/weather/WeatherCardPreview.tsx
+++ lib/settings/components/weather/WeatherCardPreview.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Client from '../../../weather/api';
+import { Forecast } from '../../../weather/interface';
 import WeatherCard from '../../../weather/components/WeatherCard';
 import { WeatherLocation } from '../../../weather/interface';
+import { useDebouncedProps } from '../../../hooks';
 
 export interface WeatherCardPreviewProps {
   readonly futurePeriod: 'Tonight'|'Tomorrow';
@@ -8,10 +11,29 @@ export interface WeatherCardPreviewProps {
   readonly location: WeatherLocation;
 }
 
-const WeatherCardPreview: React.FC<WeatherCardPreviewProps> = props => {
-  const locationDisplay = props.location.displayName || props.location.value;
+// TODO: need a way to pass the city name back up to parent so it can use it for
+// a display name if none is specified
+const WeatherCardPreview: React.FC<WeatherCardPreviewProps> = ({ location, apiKey, futurePeriod }) => {
+  const locationDisplay = location.displayName || location.value;
 
-  return <WeatherCard futurePeriod={props.futurePeriod} location={locationDisplay.toString()} />;
+  const [ currentForecast, setCurrentForecast ] = useState<Forecast>({} as any);
+  const [ futureForecast, setFutureForecast ] = useState<Forecast>({} as any);
+
+  // TODO: catch errors, add loading state, and add to WeatherCard
+  useEffect(() => {
+    Client.fetchForecasts(location, apiKey, 'F')
+      .then(({ future, current }) => {
+        setCurrentForecast(current);
+        setFutureForecast(future);
+      })
+      .catch(error => console.error('Weather fetch error', error));
+  }, [ apiKey, ...useDebouncedProps(750, location) ]);
+
+  return <WeatherCard
+    futurePeriod={futurePeriod} location={locationDisplay.toString()}
+    currentWeatherType={currentForecast.condition} currentTemperature={currentForecast.temperature}
+    futureWeatherType={futureForecast.condition} futureTemperature={futureForecast.temperature}
+  />;
 };
 
 export default WeatherCardPreview;
