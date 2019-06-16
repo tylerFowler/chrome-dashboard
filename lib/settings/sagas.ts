da63402c24c70ba944dd30c326acac5440b19d54
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-06-16 15:03:40 -0500

    Add saga to refresh current location

--- lib/settings/sagas.ts
+++ lib/settings/sagas.ts
@@ -1,6 +1,12 @@
 import { all, select, call, put, delay, takeLatest, debounce } from 'redux-saga/effects';
-import { getSerializableSettings } from './selectors';
-import { committed, commitFailure, receiveSettings, addToast, Actions, removeToast } from './actions';
+import { getSerializableSettings, getWeatherLocationConfig } from './selectors';
+import { WeatherLocation } from '../weather/interface';
+import { fetchForecastError } from '../weather/actions';
+import {
+  committed, commitFailure, receiveSettings, addToast, Actions, removeToast,
+  updateWeatherConfig,
+  commit,
+} from './actions';
 
 // TODO: create some pluggable functions for getting & setting to/from local
 // storage, export saga generator creators that inject this for writing tests
@@ -38,8 +44,33 @@ function* settingsStoredToast() {
   yield put(removeToast());
 }
 
+function* refreshCurrentLocation() {
+  const getCurrentPosition = () => new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(
+    ({ coords }) => {
+      resolve(coords);
+    }, err => reject(err),
+  ));
+
+  const locationConfig: WeatherLocation.Coords = yield select(getWeatherLocationConfig);
+
+  try {
+    const { latitude, longitude } = yield call(getCurrentPosition);
+    yield put(updateWeatherConfig({ location: {
+      ...locationConfig, value: { lat: latitude, lon: longitude },
+    }}));
+
+    yield put(commit());
+  } catch {
+    // there's no good place to put this, either we push an error to the weather
+    // module or the weather module modifies settings so for now this is where it
+    // will go
+    yield put(fetchForecastError(new Error('Unable to detect your current location')));
+  }
+}
+
 export default function* rootSaga() {
   yield call(restoreSettings);
+  yield call(refreshCurrentLocation);
   yield all([
     takeLatest(Actions.Commit, commitSettings),
     debounce(toastDebounce, Actions.UpdateFeedConfiguration, commitSettings),
