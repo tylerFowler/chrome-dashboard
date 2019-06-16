3980b6470ce9faf6170d9c133cd525050d3073ba
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-06-16 13:42:59 -0500

    Use weather unit setting in weather preview

--- lib/settings/components/weather/WeatherCardPreview.tsx
+++ lib/settings/components/weather/WeatherCardPreview.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react';
-import LocationEditorDispatch from './locationEditorDispatch';
+import { useSelector } from 'react-redux';
 import { useDebouncedProps } from 'lib/hooks';
 import * as Client from 'lib/weather/api';
+import LocationEditorDispatch from './locationEditorDispatch';
+import { getWeatherUnits } from '../../selectors';
 import { Forecast } from 'lib/weather/interface';
 import WeatherCard from 'lib/weather/components/WeatherCard';
 import { WeatherLocation } from 'lib/weather/interface';
@@ -15,6 +17,7 @@ export interface WeatherCardPreviewProps {
 const WeatherCardPreview: React.FC<WeatherCardPreviewProps> = ({ location, apiKey, futurePeriod }) => {
   const dispatch = useContext(LocationEditorDispatch);
 
+  const weatherUnit = useSelector(getWeatherUnits);
   const [ currentForecast, setCurrentForecast ] = useState<Forecast>({} as any);
   const [ futureForecast, setFutureForecast ] = useState<Forecast>({} as any);
 
@@ -24,8 +27,7 @@ const WeatherCardPreview: React.FC<WeatherCardPreviewProps> = ({ location, apiKe
     }
 
     dispatch({ type: 'forecastFetched' });
-
-    Client.fetchForecasts(location, apiKey, 'F')
+    Client.fetchForecasts(location, apiKey, weatherUnit)
       .then(({ future, current, city }) => {
         dispatch({ type: 'forecastFetchSuccess' });
 
@@ -48,7 +50,10 @@ const WeatherCardPreview: React.FC<WeatherCardPreviewProps> = ({ location, apiKe
         type: 'forecastFetchFailure',
         payload: `Failed to load weather forecast: ${error.message}`,
       }));
-  }, [ apiKey, ...useDebouncedProps(750, location.type, location.countryCode, location.value, location.displayName) ]);
+  }, [
+    apiKey, weatherUnit,
+    ...useDebouncedProps(750, location.type, location.countryCode, location.value, location.displayName),
+  ]);
 
   return <WeatherCard
     futurePeriod={futurePeriod} location={location.displayName.toString() || undefined}
