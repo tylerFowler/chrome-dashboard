7c1e1fa231c9beffc000a597bc57a41c51a1120f
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-06-16 14:16:34 -0500

    Implement forecast fetch saga

--- lib/weather/sagas.ts
+++ lib/weather/sagas.ts
@@ -1,15 +1,19 @@
-import { takeEvery, put } from 'redux-saga/effects';
+import { takeEvery, call, put, select } from 'redux-saga/effects';
 import { ActionType } from 'typesafe-actions';
-import { Actions, fetchForecast as fetchForecastAction, recvForecast } from './actions';
+import * as API from './api';
+import { Actions, fetchForecast as fetchForecastAction, recvForecast, fetchForecastError } from './actions';
+import { getWeatherAPIKey } from '../settings/selectors';
 
-function* fetchForecast(_: ActionType<typeof fetchForecastAction>) {
-  yield put(recvForecast({
-    condition: 'clearDay',
-    temperature: 80,
-  }, {
-    condition: 'clearNight',
-    temperature: 65,
-  }));
+function* fetchForecast(action: ActionType<typeof fetchForecastAction>) {
+  const apiKey = yield select(getWeatherAPIKey);
+  const { location, unit } = action.payload;
+
+  try {
+    const { current, future } = yield call(API.fetchForecasts, location, apiKey, unit);
+    yield put(recvForecast(current, future));
+  } catch (error) {
+    yield put(fetchForecastError(error));
+  }
 }
 
 export default function* rootSaga() {
