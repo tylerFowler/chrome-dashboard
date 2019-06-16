1385d07e9f0af51cc76ece46912ad4097dc8ca03
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-06-16 14:14:55 -0500

    Remove location & units from weather state

--- lib/weather/reducer.ts
+++ lib/weather/reducer.ts
@@ -1,26 +1,16 @@
 import { Actions, WeatherAction } from './actions';
-import { WeatherLocation, Forecast, WeatherLocationType } from './interface';
+import { Forecast } from './interface';
 
 export interface State {
-  readonly location: WeatherLocation;
-  readonly fetchingForecast: boolean;
-  readonly forecastFetchError: Error;
-
-  readonly unit: 'F'|'C';
   readonly currentForecast: Partial<Forecast>;
   readonly futureForecast: Partial<Forecast>;
+  readonly fetchingForecast: boolean;
+  readonly forecastFetchError: Error;
 }
 
 export const defaultState: State = {
-  location: {
-    type: WeatherLocationType.CityName,
-    value: 'Kansas City',
-    countryCode: 'US',
-    displayName: 'KC',
-  },
   fetchingForecast: false,
   forecastFetchError: null,
-  unit: 'F', // TODO: move this to settings state
   currentForecast: {},
   futureForecast: {},
 };
@@ -28,17 +18,13 @@ export const defaultState: State = {
 export default function weatherReducer(state: State = defaultState, action: WeatherAction): State {
   switch (action.type) {
   case Actions.FetchForecast:
-    return { ...state,
-      location: action.payload.location, unit: action.payload.unit,
-      fetchingForecast: true, forecastFetchError: null,
-    };
+    return { ...state, fetchingForecast: true, forecastFetchError: null };
   case Actions.FetchForecastFailure:
     return { ...state,
       fetchingForecast: false, forecastFetchError: action.payload.error,
     };
   case Actions.ReceiveForecast:
-    return { ...state,
-      fetchingForecast: false,
+    return { ...state, fetchingForecast: false,
       currentForecast: action.payload.current, futureForecast: action.payload.future,
     };
   default:
