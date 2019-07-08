b097127421cd0ad4ac7252bf90f6f0145a59774e
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-07-07 23:22:35 -0500

    Remove unused import

--- lib/PrimaryPanelPicker.tsx
+++ lib/PrimaryPanelPicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'lib/styled-components';
 import { FeedType } from './settings/interface';
