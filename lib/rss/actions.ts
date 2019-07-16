41d9bbc20313ae6fcf27131bef99df73205a210c
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-07-15 23:16:57 -0500

    Add RSS actions

--- lib/rss/actions.ts
+++ lib/rss/actions.ts
@@ -1,4 +1,5 @@
-import { ActionType } from 'typesafe-actions';
+import { action, ActionType } from 'typesafe-actions';
+import { RSSItem } from './interface';
 
 export enum Actions {
   FetchChannel = 'RSS_FETCH_CHAN',
@@ -6,4 +7,17 @@ export enum Actions {
   RefreshChannel = 'RSS_CHAN_REFRESH',
 }
 
-export type RSSAction = ActionType<any>;
+export type RSSAction = ActionType<
+  | typeof fetchRSSFeed
+  | typeof populateRSSFeed
+  | typeof rssFeedFetchFailure
+>;
+
+export const fetchRSSFeed = (feedUrl: string) =>
+  action(Actions.FetchChannel, { url: feedUrl });
+
+export const populateRSSFeed = (feedUrl: string, items: readonly RSSItem[]) =>
+  action(Actions.RefreshChannel, { url: feedUrl, items });
+
+export const rssFeedFetchFailure = (feedUrl: string, error: Error) =>
+  action(Actions.FetchChannelFailure, { url: feedUrl, error });
