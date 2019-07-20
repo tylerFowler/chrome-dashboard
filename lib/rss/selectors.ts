335a9270ec8ceeb2b3ecd05394ab24463044a1bf
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-07-19 19:35:33 -0500

    Add default value for getItems selector

--- lib/rss/selectors.ts
+++ lib/rss/selectors.ts
@@ -6,7 +6,7 @@ export const hasFeed = (url: string, { rssFeed }: State) =>
   rssFeed.channels.hasOwnProperty(url);
 
 export const getItemsForFeed = (url: string, { rssFeed }: State) =>
-  hasFeed(url, {rssFeed}) && rssFeed.channels[url].items;
+  (hasFeed(url, {rssFeed}) && rssFeed.channels[url].items) || [];
 
 export const getFeedRefreshError = (url: string, { rssFeed }: State) =>
   hasFeed(url, {rssFeed}) && rssFeed.channels[url].pullError;
