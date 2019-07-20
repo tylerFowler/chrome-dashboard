289a833d126da71bf2c8c894506b9922743452bf
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-07-19 19:13:39 -0500

    Add RSSFeedPanel

---
+++ lib/rss/components/RSSFeedPanel.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { GlobalState } from '../../store';
+import { isFetchingFeed, getFeedRefreshError, getItemsForFeed } from '../selectors';
+import { RSSItem } from '../interface';
+import FeedItem from '../../panel/components/FeedItem';
+import FeedPanel, { FeedProps } from '../../panel/components/FeedPanel';
+import { useSelector } from 'react-redux';
+
+export interface RSSFeedPanelProps extends FeedProps {
+  readonly feedUrl: string;
+  readonly maxItems: number;
+}
+
+const RSSFeedPanel: React.FC<RSSFeedPanelProps> = ({ feedUrl, maxItems = 10, title, ...panelProps }) => {
+  const rssFeedName = title;
+
+  const isFetching = useSelector<GlobalState, boolean>(state => isFetchingFeed(feedUrl, state));
+  const fetchError = useSelector<GlobalState, Error>(state => getFeedRefreshError(feedUrl, state));
+  const feedItems = useSelector<GlobalState, readonly RSSItem[]>(state => getItemsForFeed(feedUrl, state));
+
+  return (
+    <FeedPanel {...panelProps} title={rssFeedName} loading={isFetching} fetchError={fetchError}>
+      {feedItems.map((item, idx) =>
+        <li key={item.guid}>
+          <FeedItem
+            id={item.guid}
+            index={idx + 1}
+            title={item.title}
+            url={item.link}
+            author={item.author}
+          />
+        </li>,
+      )}
+    </FeedPanel>
+  );
+};
+
+export default RSSFeedPanel;
