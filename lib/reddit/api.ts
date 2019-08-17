1238fb29c713b2c42150b42dd9e5a22e29fb1d32
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-08-17 17:42:30 -0500

    Stub out reddit API func

---
+++ lib/reddit/api.ts
@@ -0,0 +1,22 @@
+import { RedditPost, FeedType } from './interface';
+
+export const RedditAPI = 'https://reddit.com/';
+
+export const isCacheableRequest = (request: Request): boolean =>
+  request.url.startsWith(RedditAPI);
+
+interface SubRedditFeedResponse {
+  id: string;
+  title: string;
+  author: string;
+  ups: number;
+  url: string;
+  num_comments: number;
+  created: number;
+}
+
+export default async function fetchSubreddit(
+  subreddit: string, feedType: FeedType, pullSize: number,
+): Promise<RedditPost[]> {
+  throw new Error('not implemented');
+}
