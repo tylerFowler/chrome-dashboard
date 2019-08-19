ba9501d8b4e0b22076940eadc8a80b1af2997ec8
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-08-19 16:08:03 -0500

    Add IDs to subreddit settings form

--- lib/settings/components/feeds/SubredditSettings.tsx
+++ lib/settings/components/feeds/SubredditSettings.tsx
@@ -11,20 +11,21 @@ export interface SubredditSettingsProps {
 const SubredditSettings: React.FC<{ readonly panelOrientation: PanelOrientation }> = ({ panelOrientation }) => {
   // TODO: get the current values from settings state, ignore the setters - we'll only dispatch things
 
-  // TODO: add id's to these, going to have to make a func called makeId that appends the orientation
+  const makeId = (id: string) => `${id}-${panelOrientation}`;
+
   // TODO: when sub name is filled out, attempt a request to the subreddit and report any 404 errors
   //       and attempt to find a theme color for the sub, using that as the theme color
   // TODO: pull out the feed type options into generic component, will need it again later
   // TODO: add input for display name, use the request data to fill out the full subreddit name by default
   return (<>
     <SettingField>
-      <SettingInlineLabel>Sub Name:</SettingInlineLabel>
-      <SettingInput value="" />
+      <SettingInlineLabel htmlFor={makeId('subreddit-name')}>Sub Name:</SettingInlineLabel>
+      <SettingInput id={makeId('subreddit-name')} value="" />
     </SettingField>
 
     <SettingField>
-      <SettingInlineLabel>Default Feed Type:</SettingInlineLabel>
-      <SettingSelect value="">
+      <SettingInlineLabel htmlFor={makeId('default-subreddit-feed-type')}>Default Feed Type:</SettingInlineLabel>
+      <SettingSelect id={makeId('default-subreddit-feed-type')} value="">
         <option value={FeedType.Top} defaultChecked={true}>top</option>
         <option value={FeedType.New}>new</option>
         <option value={FeedType.Rising}>rising</option>
@@ -34,8 +35,8 @@ const SubredditSettings: React.FC<{ readonly panelOrientation: PanelOrientation 
     </SettingField>
 
     <SettingField>
-      <SettingInlineLabel>Theme:</SettingInlineLabel>
-      <PanelThemeInput />
+      <SettingInlineLabel htmlFor={makeId('subreddit-panel-theme-color')}>Theme:</SettingInlineLabel>
+      <PanelThemeInput id={makeId('subreddit-panel-theme-color')} />
     </SettingField>
   </>);
 };
