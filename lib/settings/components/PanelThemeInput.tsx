b06c26a6f6b5d1590c213a6e20425b2cfc07703d
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-08-19 16:36:00 -0500

    Allow values to be passed as a full theme

--- lib/settings/components/PanelThemeInput.tsx
+++ lib/settings/components/PanelThemeInput.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import hexToRGB from 'hex-rgb';
-import { PanelThemeCustomization } from '../../panel/panelTheme';
+import { defaultTheme, PanelThemeCustomization } from '../../panel/panelTheme';
 import { SettingInput } from './SettingsForm';
 
-type InputHTMLAttributeSubset = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'|'onChange'>;
+type InputHTMLAttributeSubset = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'|'onChange'|'value'>;
 
 export interface PanelThemeInputProps extends InputHTMLAttributeSubset {
-  onChange?(updatedTheme: Partial<PanelThemeCustomization>): void;
+  readonly value: string|PanelThemeCustomization;
+  onChange?(updatedTheme: PanelThemeCustomization): void;
 }
 
 const customInputCSS: React.CSSProperties = {
@@ -15,23 +16,31 @@ const customInputCSS: React.CSSProperties = {
   verticalAlign: 'sub',
 };
 
+const valueIsTheme = (value: PanelThemeInputProps['value']): value is PanelThemeCustomization =>
+  value.hasOwnProperty('primaryColor');
+
 // PanelThemeInput is an input that can be used to choose a color scheme to use
 // for a Panel, generating the theme on the fly.
 //
 // TODO: need to decide on whether to use the light font or dark font based on
 // the color, can probably just avg together the RGB values, using dark font
 // or light font at some threshold of brightness.
-const PanelThemeInput: React.FC<PanelThemeInputProps> = ({ onChange, ...inputProps}) => {
+const PanelThemeInput: React.FC<PanelThemeInputProps> = ({ onChange, value, ...inputProps}) => {
+  const inputValue = valueIsTheme(value)
+    ? value.primaryColor
+    : value || '';
+
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const hexColor = event.target.value;
 
     onChange({
       primaryColor: hexColor,
       primaryColorRGB: hexToRGB(hexColor),
+      fontColor: defaultTheme.fontColor,
     });
   };
 
-  return <SettingInput {...inputProps} type="color" onChange={onInputChange}
+  return <SettingInput {...inputProps} type="color" onChange={onInputChange} value={inputValue}
     style={{ ...customInputCSS, ...inputProps.style }}
   />;
 };
