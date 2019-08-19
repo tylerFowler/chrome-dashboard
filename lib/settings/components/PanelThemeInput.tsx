776111f2631840ff19f056b079d22c97b761750d
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-08-19 17:20:59 -0500

    Set font value based on perceived lightness

--- lib/settings/components/PanelThemeInput.tsx
+++ lib/settings/components/PanelThemeInput.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import hexToRGB from 'hex-rgb';
-import { defaultTheme, PanelThemeCustomization } from '../../panel/panelTheme';
+import { defaultTheme, PanelThemeCustomization, RGB } from '../../panel/panelTheme';
 import { SettingInput } from './SettingsForm';
 
 type InputHTMLAttributeSubset = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'|'onChange'|'value'>;
@@ -19,6 +19,13 @@ const customInputCSS: React.CSSProperties = {
 const valueIsTheme = (value: PanelThemeInputProps['value']): value is PanelThemeCustomization =>
   value && value.hasOwnProperty('primaryColor');
 
+// getLightnessValue computes a percentage decimal from 0 to 1 on how "light" a
+// given RGB value is, using the perceived lightness (Luma) calculations based on
+// the sRGB Luma algorithm:
+// https://css-tricks.com/switch-font-color-for-different-backgrounds-with-css/#article-header-id-6
+const getLightnessValue = ({ red, green, blue }: RGB): number =>
+  (red * .2126 + green * 0.7152 + blue * 0.0722) / 255;
+
 // PanelThemeInput is an input that can be used to choose a color scheme to use
 // for a Panel, generating the theme on the fly.
 //
@@ -32,11 +39,14 @@ const PanelThemeInput: React.FC<PanelThemeInputProps> = ({ onChange, value, ...i
 
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const hexColor = event.target.value;
+    const rgbColor = hexToRGB(hexColor);
 
     onChange({
       primaryColor: hexColor,
-      primaryColorRGB: hexToRGB(hexColor),
-      fontColor: defaultTheme.fontColor,
+      primaryColorRGB: rgbColor,
+      fontColor: getLightnessValue(rgbColor) > .5
+        ? defaultTheme.typeDark
+        : defaultTheme.typeLight,
     });
   };
 
