7ec97e55f577b477e3419fadf4ad4c481fc71970
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-09-02 03:31:46 -0500

    Add display string converter for hn feed type

--- lib/hn/interface.ts
+++ lib/hn/interface.ts
@@ -4,3 +4,20 @@ export enum FeedType {
   BestStories = 'beststories',
   ShowStories = 'showstories',
 }
+
+export namespace FeedType {
+  export function getDisplayString(feed: FeedType) {
+    switch (feed) {
+    case FeedType.TopStories:
+      return 'top';
+    case FeedType.NewStories:
+      return 'new';
+    case FeedType.BestStories:
+      return 'best';
+    case FeedType.ShowStories:
+      return 'show';
+    default:
+      return feed as string;
+    }
+  }
+}
