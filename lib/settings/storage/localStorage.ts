88fe6d27d4495db4be6cd7b53ba6a26a04911c66
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-09-15 18:25:50 -0500

    Allow any web API storage implementation to be used

--- lib/settings/storage/localStorage.ts
+++ lib/settings/storage/localStorage.ts
@@ -3,13 +3,18 @@ import { SettingsStore } from './index';
 
 export default class LocalStorageSettingsStore implements SettingsStore {
   private static storageKey = 'settings';
+  private storage: Storage;
+
+  constructor(storageMechanism: Storage = localStorage) {
+    this.storage = storageMechanism;
+  }
 
   public async commitSettings(settings: Settings) {
-    localStorage.setItem(LocalStorageSettingsStore.storageKey, JSON.stringify(settings));
+    this.storage.setItem(LocalStorageSettingsStore.storageKey, JSON.stringify(settings));
   }
 
   public async restoreSettings() {
-    const settingsString = localStorage.getItem(LocalStorageSettingsStore.storageKey);
+    const settingsString = this.storage.getItem(LocalStorageSettingsStore.storageKey);
 
     return JSON.parse(settingsString);
   }
