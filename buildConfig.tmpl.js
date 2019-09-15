0426ca95da8c395374a254c1621fb2c5c96974a9
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-09-15 18:14:40 -0500

    Add settings storage to build configuration

--- buildConfig.tmpl.js
+++ buildConfig.tmpl.js
@@ -3,5 +3,10 @@ export default {
   // can be obtained from https://openweathermap.org/api by signing up for an account.
   // It's likely that, if being used for personal use only, the free tier will be enough
   // as the extension caches forecast data for ~5 minutes before making another API request.
-  openweatherAPIKey: "<OPENWEATHER_API_KEY>"
+  openweatherAPIKey: "<OPENWEATHER_API_KEY>",
+
+  // Determines the storage mechanism that can be used for settings persistence. By
+  // default the value is "localStorage". The possible values are
+  // - localStorage: the browser's local storage API will be used to store settings
+  settingsStorage: "localStorage",
 };
