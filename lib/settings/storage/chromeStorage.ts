63911ec692415bc93131990d57b0e69514f460d4
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-09-15 19:03:09 -0500

    Implement settings storage using Chrome API

--- lib/settings/storage/chromeStorage.ts
+++ lib/settings/storage/chromeStorage.ts
@@ -2,11 +2,31 @@ import { State as Settings } from '../reducer';
 import { SettingsStore } from './index';
 
 export default class ChromeStorageSettingsStore implements SettingsStore {
-  public async commitSettings(settings: Settings) {
-    throw new Error('not implemented');
+  private static storageKey = 'settings';
+
+  public commitSettings(settings: Settings): Promise<void> {
+    const updateObject = {
+      [ChromeStorageSettingsStore.storageKey]: JSON.stringify(settings),
+    };
+
+    return new Promise((resolve, reject) => chrome.storage.sync.set(updateObject, () => {
+      if (chrome.runtime.lastError) {
+        return reject(new Error(chrome.runtime.lastError.message));
+      }
+
+      return resolve();
+    }));
   }
 
-  public async restoreSettings() {
-    return {}
+  public async restoreSettings(): Promise<Settings> {
+    return new Promise((resolve, reject) =>
+      chrome.storage.sync.get(ChromeStorageSettingsStore.storageKey, items => {
+        if (chrome.runtime.lastError) {
+          return reject(new Error(chrome.runtime.lastError.message));
+        }
+
+        return resolve(JSON.parse(items[ChromeStorageSettingsStore.storageKey]) as Settings);
+      }),
+    );
   }
 }
