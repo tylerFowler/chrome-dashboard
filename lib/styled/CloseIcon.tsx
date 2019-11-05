0c74befe83d031ad43e1072c9895241bc8c9eac0
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-11-04 18:55:04 -0600

    Allow close icon to be configurable

--- lib/styled/CloseIcon.tsx
+++ lib/styled/CloseIcon.tsx
@@ -1,17 +1,19 @@
 import styled from 'lib/styled-components';
 import React from 'react';
+import { AppTheme } from '../theme';
 
 export interface CloseIconProps {
-  readonly size: string;
-  readonly thickness: string;
-  readonly color: string;
+  readonly size?: string;
+  readonly thickness?: string;
+  readonly iconColor?: (theme: AppTheme) => string;
+  readonly style?: React.CSSProperties;
 }
 
-const Icon = styled.div.attrs<any, CloseIconProps>({
-  size: '1.5rem',
-  thickness: '2px',
-  color: '',
-})`
+const Icon = styled.div.attrs<CloseIconProps, CloseIconProps>(props => ({
+  size: props.size || '1.5rem',
+  thickness: props.thickness || '2px',
+  iconColor: props.iconColor || (theme => theme.backgroundExtraLight),
+}))`
   cursor: pointer;
   display: inline-block;
   float: right;
@@ -27,13 +29,13 @@ const Icon = styled.div.attrs<any, CloseIconProps>({
     height: ${props => props.size};
 
     position: relative;
-    background-color: ${props => props.color || props.theme.backgroundExtraLight};
+    background-color: ${props => props.iconColor(props.theme)};
     transform-origin: center;
   }
 
   // Compensate both of these for the shapes bleeding over the left edge of
   // their container
-  &::before { transform: rotate(45deg); left: calc(2px + .5em); }
+  &::before { transform: rotate(45deg); left: calc(${props => props.thickness} + .5em); }
   &::after  { transform: rotate(-45deg); left: .5em; }
 `;
 
