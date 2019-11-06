6c6d24a96e26c6cc21db7f6afafbda44f1f42a76
Tyler Fowler <tylerfowler.1337@gmail.com>
2019-11-06 13:56:32 -0600

    Implement restoreProgress saga

--- lib/onboarding/sagas.ts
+++ lib/onboarding/sagas.ts
@@ -1,15 +1,29 @@
 import { all, select, call, put, delay, takeLatest, debounce } from 'redux-saga/effects';
+import applicationStore from 'lib/storage';
 import { Actions } from './actions';
 import * as action from './actions';
+import { SerializableOnboardingProgress, deserializeProgress } from './selectors';
+
+const storageKey = 'onboardingProgress';
 
 function* storeProgress() { yield new Error('not implemented'); }
 
 function* restoreProgress() {
   try {
-    // TODO: load & deserialize
+    const serializedProgress: SerializableOnboardingProgress = yield call(applicationStore.getData, storageKey);
+
+    if (serializedProgress) {
+      const deserializedProgress = deserializeProgress(serializedProgress);
+      yield put(action.restoreProgress(deserializedProgress));
+    }
   } catch (err) {
     console.warn('Unable to restore onboarding progress:', err);
     yield put(action.progressRestorationFailure(err));
+
+    // if we can't know the user's progression through onboarding, err on the side
+    // of not annoying the user with messages that they *may* have seen
+    console.warn('Disabling onboarding as progress is unknown');
+    yield put(action.disableOnboarding());
   }
 }
 
